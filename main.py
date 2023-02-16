1515a4334c3de664cef3a3ac99afb75a572fc8a2
A00466017 <abhishek.vijayakumar.latha@smu.ca>
2023-02-15 20:00:04 -0400

    Deploying with input

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from joblib import load
-import streamlit
-streamlit.title('YOUR APP')
+import streamlit as st
+# st.title('YOUR APP')
 
-streamlit.title("Deploying the model")
+st.title("Deploying the model")
+
+LABELS = ['setosa', 'versicolor', 'virginica']
 
 clf = load("DT.joblib")
+
+sp_l = st.slide('sepal length (cm)', min_value=0, max_value=18)
+
+sp_w = st.slide('sepal width (cm)', min_value=0, max_value=18)
+
+pe_l = st.slide('petal length (cm)', min_value=0, max_value=18)
+
+pe_w = st.slide('petal width (cm)', min_value=0, max_value=18)
+
+prediction = clf.predict([[sp_l, sp_w, pe_l, pe_w]])
+
+st.write(LABELS[prediction])
