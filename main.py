20a153c052271d138a0267cb34370c03babcd47e
A00466017 <abhishek.vijayakumar.latha@smu.ca>
2023-02-15 20:14:15 -0400

    Fixed bugs

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ LABELS = ['setosa', 'versicolor', 'virginica']
 
 clf = load("DT.joblib")
 
-sp_l = st.slide('sepal length (cm)', min_value=0, max_value=18)
+sp_l = st.slider('sepal length (cm)', min_value=0, max_value=18)
 
-sp_w = st.slide('sepal width (cm)', min_value=0, max_value=18)
+sp_w = st.slider('sepal width (cm)', min_value=0, max_value=18)
 
-pe_l = st.slide('petal length (cm)', min_value=0, max_value=18)
+pe_l = st.slider('petal length (cm)', min_value=0, max_value=18)
 
-pe_w = st.slide('petal width (cm)', min_value=0, max_value=18)
+pe_w = st.slider('petal width (cm)', min_value=0, max_value=18)
 
 prediction = clf.predict([[sp_l, sp_w, pe_l, pe_w]])
 
